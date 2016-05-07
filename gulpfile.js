59d17b823f59bbaf14b26ea3d824d2cf450ef56d
João Marcus de Lemos Fernandes <jmldesignergrafico@gmail.com>
2016-05-07 15:08:09 -0300

    Prepare Site for jekyll

--- gulpfile.js
+++ gulpfile.js
@@ -4,25 +4,79 @@ var gulp = require('gulp'),
 	less = require('gulp-less'),
 	path = require('path'),
 	cleanCSS = require('gulp-clean-css'),
-	cssbeautify = require('gulp-cssbeautify');
+	cssBeautify = require('gulp-cssbeautify'),
+      runSequence = require('run-sequence'),
+      cssComb = require('gulp-csscomb'),
+      sass = require('gulp-ruby-sass');
 
-gulp.task('concat', function() {
-  return gulp.src('./js/scripts/*.js')
+gulp.task('default', function (callback) { // Executa sequecia de tarefas principais do Gulp
+  return runSequence('less', ['clean:css','concat:css', 'concat:js'], callback);
+});
+
+gulp.task('hint:js', function() {
+  return gulp.src('./assets/js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('concat:js', function() { // Concatenação de arquivos JS
+    return gulp.src('./assets/js/*.js')
     .pipe(concat('main.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('./js/'));
+    .pipe(gulp.dest('./lib/'));
+
+});
+
+gulp.task('concat:css', function() { // Concatenação de arquivos CSS
+    return gulp.src('./assets/css/*.css')
+    .pipe(concat('main.css'))
+    .pipe(cssBeautify({
+      indent: '  ',
+      openbrace: 'separate-line',
+      autosemicolon: true
+    }))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(cssComb())
+    .pipe(gulp.dest('./lib/'));
 });
 
 gulp.task('less', function () {
-  return gulp.src('./less/**/custom.less')
+  return gulp.src('./assets/less/**/custom.less')
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
-    .pipe(gulp.dest('css/'))
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(cssbeautify());
+    .pipe(gulp.dest('assets/css/'))
+    .pipe(cssBeautify());
 });
 
-gulp.task('less:watch', function () {
-  gulp.watch('less/**/*.less',['less']);
+
+gulp.task('watch:less', function () {
+  gulp.watch('assets/less/**/*.less',['less']);
+});
+
+gulp.task('clean:css', function () { // Limpa CSS
+    return gulp.src('./assets/css/*.css')
+    .pipe(cssBeautify({
+      indent: '  ',
+      openbrace: 'separate-line',
+      autosemicolon: true
+    }))
+   .pipe(cssComb())
+   .pipe(gulp.dest('assets/css/'));
+});
+
+gulp.task('watch:css', function () { // Assistir modificações em CSS
+    gulp.watch('assets/css/*.css',['clean:css', 'concat:css']);
+});
+
+gulp.task('watch:js', function () { // Assistir modificações em JS
+    gulp.watch('assets/js/*.js',['concat:js']);
+});
+
+gulp.task('watch:all', function () { // Assiste modificações em todos os arquivos de assets
+  gulp.watch([
+            'assets/**/*.less',
+            'assets/**/*.css',
+            'assets/**/*.js',
+             ], ['default']);
 });
