f39bab11c559e66760ac5928f6d6a909fdbec478
João Marcus de Lemos Fernandes <jmldesignergrafico@gmail.com>
2016-08-28 23:17:34 -0300

    Fixed not found URL when Root URL

--- app/config/run.js
+++ app/config/run.js
@@ -1,8 +1,8 @@
 angular
 	.module('joaomarcuslfWebApp')
 	.run(function($rootScope, $location) {
-	    $rootScope.$on("$locationChangeStart", function(event, next, current) { 
-	        if($location.$$path === '/home') {
+	    $rootScope.$on("$locationChangeStart", function(event, next, current) {
+	        if($location.$$path === '/home' || $location.$$path === '') {
 	        	$rootScope.pageTitle = "Home"
 	        	$rootScope.page = 'home';
 	        } else if($location.$$path === '/about') {
