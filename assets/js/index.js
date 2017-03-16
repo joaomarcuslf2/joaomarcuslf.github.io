1cd775bad6bf7e101b18a8efab1a05f826029422
João Marcus de Lemos Fernandes <jmldesignergrafico@gmail.com>
2017-03-15 21:52:55 -0300

    Changed js files to fit eslint

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,90 +1,90 @@
-import './pollyfils.js';
-
-function closeNavigation() {
-		let navigation = document.querySelector('.navigation');
-		let openNavBtn = document.querySelector('.open-nav-btn');
-		let overlay = document.querySelector('#overlay');
-		let appContainer = document.getElementById('appContainer');
-
-		navigation.className = navigation
-				.className
-				.split(' open-nav')
-				.join('');
-		openNavBtn.className = openNavBtn
-				.className
-				.split(' is-open')
-				.join('');
-		appContainer.className = 'open';
-		overlay.className = '';
-}
-
-function toggleSideBarMenu() {
-		let appContainer = document.getElementById('appContainer');
-
-		if (appContainer.className === 'open') {
-				appContainer.className = '';
-		} else {
-				appContainer.className = 'open';
-		}
-}
-
-function toggleNavigation() {
-		let navigation = document.querySelector('.navigation');
-		let openNavBtn = document.querySelector('.open-nav-btn');
-		let overlay = document.querySelector('#overlay');
-		let appContainer = document.getElementById('appContainer');
-
-		if (navigation.className.includes('open-nav')) {
-				closeNavigation();
-		} else {
-				navigation.className += ' open-nav';
-				openNavBtn.className += ' is-open';
-				overlay.className = 'open overlay open-overlay no-overflow';
-				appContainer.className += ' ' + overlay.className;
-
-				openNavBtn.blur();
-		}
-}
-
-document
-		.querySelector('.open-btn')
-		.addEventListener('click', toggleSideBarMenu, false);
-
-document
-		.querySelector('.open-nav-btn')
-		.addEventListener('click', toggleNavigation, false);
-
-document
-		.querySelector('#overlay')
-		.addEventListener('click', closeNavigation, false);
-
-document
-		.querySelectorAll('.link-item')
-		.forEach((element) => element.addEventListener('click', closeNavigation, false));
-
+import './pollyfils';
 import portfolioItens from './portfolio.json';
-let portfolioContainer = document.querySelector('.portfolio-container');
+
+const closeNavigation = () => {
+  const navigation = document.querySelector('.navigation');
+  const openNavBtn = document.querySelector('.open-nav-btn');
+  const overlay = document.querySelector('#overlay');
+  const appContainer = document.getElementById('appContainer');
+
+  navigation.className = navigation
+    .className
+    .split(' open-nav')
+    .join('');
+  openNavBtn.className = openNavBtn
+    .className
+    .split(' is-open')
+    .join('');
+  appContainer.className = 'open';
+  overlay.className = '';
+};
+
+const toggleSideBarMenu = () => {
+  const appContainer = document.getElementById('appContainer');
+
+  if (appContainer.className === 'open') {
+    appContainer.className = '';
+  } else {
+    appContainer.className = 'open';
+  }
+};
+
+const toggleNavigation = () => {
+  const navigation = document.querySelector('.navigation');
+  const openNavBtn = document.querySelector('.open-nav-btn');
+  const overlay = document.querySelector('#overlay');
+  const appContainer = document.getElementById('appContainer');
+
+  if (navigation.className.includes('open-nav')) {
+    closeNavigation();
+  } else {
+    navigation.className += ' open-nav';
+    openNavBtn.className += ' is-open';
+    overlay.className = 'open overlay open-overlay no-overflow';
+    appContainer.className += ` ${overlay.className}`;
+
+    openNavBtn.blur();
+  }
+};
+
+document
+  .querySelector('.open-btn')
+  .addEventListener('click', toggleSideBarMenu, false);
+
+document
+  .querySelector('.open-nav-btn')
+  .addEventListener('click', toggleNavigation, false);
+
+document
+  .querySelector('#overlay')
+  .addEventListener('click', closeNavigation, false);
+
+document
+  .querySelectorAll('.link-item')
+  .forEach(element => element.addEventListener('click', closeNavigation, false));
+
+const portfolioContainer = document.querySelector('.portfolio-container');
 
 portfolioItens
-		.items
-		.map((item) => {
-				let img = document.createElement('img');
-				let figure = document.createElement('figure');
-				let label = document.createElement('h2');
-				let p = document.createElement('p');
-				let referenceLink = document.createElement('a');
+  .items
+  .forEach((item) => {
+    const img = document.createElement('img');
+    const figure = document.createElement('figure');
+    const label = document.createElement('h2');
+    const p = document.createElement('p');
+    const referenceLink = document.createElement('a');
 
-				img.src = '/build/img/portfolio/' + item.img;
-				figure.className = 'image is-16by9 portfolio-image';
-				p.className = 'panel-block';
-				label.innerText = item.label;
-				referenceLink.href = item.link;
-				referenceLink.className = 'button is-primary is-outlined is-medium	is-fullwidth';
+    img.src = `/build/img/portfolio/${item.img}`;
+    figure.className = 'image is-16by9 portfolio-image';
+    p.className = 'panel-block';
+    label.innerText = item.label;
+    referenceLink.href = item.link;
+    referenceLink.className = 'button is-primary is-outlined is-medium is-fullwidth';
 
-				figure.append(img);
-				referenceLink.append(label);
-				p.append(referenceLink);
-				p.append(figure);
+    figure.append(img);
+    referenceLink.append(label);
+    p.append(referenceLink);
+    p.append(figure);
 
-				portfolioContainer.append(p);
-		});
+    portfolioContainer.append(p);
+  });
--- assets/js/pollyfils.js
+++ assets/js/pollyfils.js
@@ -1,46 +1,63 @@
-if (!Array.prototype.map) {
-		Array.prototype.map = function (callback, thisArg) {
-				let T, A, k;
-				if (this == null) {
-						throw new TypeError(' this is null or not defined');
-				}
-				let O = Object(this);
-				let len = O.length >>> 0;
-				if (typeof callback !== 'function') {
-						throw new TypeError(callback + ' is not a function');
-				}
-				if (arguments.length > 1) {
-						T = thisArg;
-				}
-				A = new Array(len);
-				k = 0;
-				while (k < len) {
-						let kValue,
-								mappedValue;
-						if (k in O) {
-								kValue = O[k];
-								mappedValue = callback.call(T, kValue, k, O);
-								A[k] = mappedValue;
-						}
-						k++;
-				}
+/* eslint no-tabs: 0 */
+/* eslint indent: 0 */
+/* eslint no-undef: 0 */
+/* eslint no-plusplus: 0 */
+/* eslint prefer-template: 0 */
+/* eslint prefer-const: 0 */
+/* eslint one-var: 0 */
+/* eslint no-extend-native: 0 */
+/* eslint func-names: 0 */
+/* eslint one-var-declaration-per-line: 0 */
+/* eslint no-bitwise: 0 */
+/* eslint space-in-parens: 0 */
+/* eslint keyword-spacing: 0 */
+/* eslint space-before-function-paren: 0 */
 
-				return A;
-		};
+if (!Array.prototype.map) {
+  Array.prototype.map = function (callback, thisArg) {
+  let T,
+A,
+k;
+  if (this == null) {
+  throw new TypeError(' this is null or not defined');
+}
+  const O = Object(this);
+  const len = O.length >>> 0;
+  if (typeof callback !== 'function') {
+  throw new TypeError(`${callback} is not a function`);
+}
+  if (arguments.length > 1) {
+  T = thisArg;
+}
+  A = new Array(len);
+  k = 0;
+  while (k < len) {
+  let kValue,
+  mappedValue;
+  if (k in O) {
+  kValue = O[k];
+  mappedValue = callback.call(T, kValue, k, O);
+  A[k] = mappedValue;
+}
+  k++;
 }
 
-if ( !Array.prototype.forEach ) {
-  Array.prototype.forEach = function(fn, scope) {
-    for(let i = 0, len = this.length; i < len; ++i) {
+  return A;
+};
+}
+
+if (!Array.prototype.forEach) {
+  Array.prototype.forEach = function (fn, scope) {
+    for (let i = 0, len = this.length; i < len; ++i) {
       fn.call(scope, this[i], i, this);
     }
   };
 }
 
 if (!NodeList.prototype.forEach) {
-	NodeList.prototype.forEach = Array.prototype.forEach;
+  NodeList.prototype.forEach = Array.prototype.forEach;
 }
 
 if (!Element.prototype.append) {
-	Element.prototype.append = Element.prototype.appendChild;
+  Element.prototype.append = Element.prototype.appendChild;
 }
